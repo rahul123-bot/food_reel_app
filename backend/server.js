a8e70aceb2d2373798127a6731a2c264d9ffdd15
rahul <rahulsil930@gmail.com>
2026-05-05 22:14:57 +0530

    fix server start

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const app=require("./src/app");
 require('dotenv').config();
 
 const connectDB=require('./src/db/db');
+const PORT = process.env.PORT || 5000;
+
 connectDB();
-app.listen(5000,()=>{
-    console.log("server is running on port 5000");
+app.listen(port,()=>{
+    console.log(`server is running on port ${PORT}`);
 })
